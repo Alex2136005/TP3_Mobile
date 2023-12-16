53b9555bfeb14dea24e0ba62e97a25934664f544
Mathys Deshaies <mathys.deshaies@icloud.com>
2023-12-16 18:03:31 -0500

    getjsonfromurl

--- src/customUtils.cpp
+++ src/customUtils.cpp
@@ -2,11 +2,12 @@
 
 String customUtils::getJSONFromURL(String url) const {
     HTTPClient http;
-    http.begin(url);
+    http.begin("https://api.github.com/users/hadley/repos");
     http.addHeader("Accept", "application/json");
     http.addHeader("Content-Type", "application/json");
     int httpCode = http.GET();
     String payload = http.getString();
+    http.end();
     if (httpCode == HTTP_CODE_OK) {
         Serial.println("------data------");
         Serial.println(httpCode);
@@ -14,11 +15,29 @@ String customUtils::getJSONFromURL(String url) const {
         Serial.println("------close------");
     } else {
         Serial.println("HTTP request failed");
-        Serial.println(httpCode);
+        Serial.println("HTTP Code: " + String(httpCode));
     }
 
     http.end();
 
     return payload;
+    /*CURL* curl = curl_easy_init();
+    std::string result;
+
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &result);
+
+        CURLcode res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK) {
+            std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
+        }
+
+        curl_easy_cleanup(curl);
+    }
+    return result;*/
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,9 @@ String json;
 void setup() {
   Serial.begin(115200);
   json = utils.getJSONFromURL("https://staging.revolvair.org/api/revolvair/stations/cegep-de-ste-foy/");//config::URL_REVOLVAIR
+  Serial.println("--------");
   Serial.println(json);
+  Serial.println("--------");
 }
 
 void loop() {
