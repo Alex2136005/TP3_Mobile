39b6fe32aa47e4acfde3bdd371a7df1fc03530fa
Mathys Deshaies <mathys.deshaies@icloud.com>
2023-12-19 21:32:26 -0500

    changement en temps relle test

--- lib/WifiManager/src/wifiManager.cpp
+++ lib/WifiManager/src/wifiManager.cpp
@@ -11,15 +11,7 @@ String WifiManager::getMacAddress()
 }
 String WifiManager::getUniqueId()
 {
-    byte mac[6];
-    WiFi.macAddress(mac);
-    return 
-        String(mac[0],HEX) +
-        String(mac[1],HEX) +
-        String(mac[2],HEX) +
-        String(mac[3],HEX) + 
-        String(mac[4],HEX) + 
-        String(mac[5],HEX);
+    return String(ESP.getEfuseMac());
 }
 String WifiManager::getWifiRSSI()
 {
